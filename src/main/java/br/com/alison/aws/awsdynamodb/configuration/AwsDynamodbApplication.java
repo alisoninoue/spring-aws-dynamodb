69a8d50b04e581ffc6a7da0a2de8a3a33564e688
Alison Yutaka Inoue <alison.inoue@gmail.com>
2021-11-03 22:58:03 -0300

    Move AwsDynamodbApplication.java to configuration package

--- src/main/java/br/com/alison/aws/awsdynamodb/configuration/AwsDynamodbApplication.java
+++ src/main/java/br/com/alison/aws/awsdynamodb/configuration/AwsDynamodbApplication.java
@@ -1,10 +1,11 @@
-package br.com.alison.aws.awsdynamodb;
+package br.com.alison.aws.awsdynamodb.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.alison.aws.awsdynamodb")
 public class AwsDynamodbApplication {
 
 	public static void main(String[] args) {
